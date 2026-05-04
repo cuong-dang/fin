4bd970a986a0882bf30f71a7beb133926f3a7d52
Cuong Dang <cuongd@pm.me>
2026-05-04 17:21:35 -0500

    Review web root files

---
+++ apps/web/src/theme.ts
@@ -0,0 +1,41 @@
+import {
+  ActionIcon,
+  Card,
+  createTheme,
+  Group,
+  Stack,
+  Text,
+} from "@mantine/core";
+
+export const theme = createTheme({
+  components: {
+    ActionIcon: ActionIcon.extend({
+      defaultProps: {
+        size: "xs",
+        color: "black",
+        variant: "subtle",
+      },
+    }),
+    Card: Card.extend({
+      defaultProps: {
+        withBorder: true,
+        padding: "xs",
+      },
+    }),
+    Group: Group.extend({
+      defaultProps: {
+        gap: "xs",
+      },
+    }),
+    Stack: Stack.extend({
+      defaultProps: {
+        gap: "xs",
+      },
+    }),
+    Text: Text.extend({
+      defaultProps: {
+        size: "sm",
+      },
+    }),
+  },
+});
